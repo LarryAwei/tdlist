4051ae666018e58bfd0c1b7fb86c5443f9ce3861
zihan <weizh204@163.com>
2019-10-02 01:39:26 +0800

    simple todo list finished.

--- todolist/src/newTodoList.js
+++ todolist/src/newTodoList.js
@@ -1,36 +1,59 @@
 import React, {Component} from "react";
-import store from "./store";
+
 import {connect} from 'react-redux';
-class TodoList  extends Component{
-    render() {
-        return(
+//一个组件只有render，那可以写成无状态组件，其没有生命周期函数，性能高
+const TodoList = (props) => {
+    const {inputValue, list, changeInputValue, handleClick, handleDel} = props;
+    return(
+        <div>
             <div>
-                <div>
-                    <input defaultValue={this.props.inputValue}/>
-                    <button onClick={this.handleClick}>提交</button>
-                </div>
-                <ul>
-                    <li>dell</li>
-                </ul>
+                <input value={inputValue} onChange={changeInputValue}/>
+                <button onClick={handleClick}>提交</button>
             </div>
-        )
-    }
-    handleInputChange(e) {
-        console.log(e.target.value);
-    }
+            <ul>
+                {list.map((item,index)=>{
+                    return <li key={index} onClick={() => {handleDel(index)}}>{item}</li>
+                })}
+            </ul>
+        </div>
+    )
+};
 
-}
-//store.dispatch
-const mapDispatchToProps = () => {
+//store.dispatch和props。input值发生变化时，我们想改变store内容
+const mapDispatchToProps = (dispatch) => {
     return {
+        changeInputValue(e){
+            const action = {
+                type: 'change_input_value',
+                value: e.target.value
+            };
+            //console.log(e.target.value);
+            dispatch(action)
 
+        },
+        handleClick(){
+            const action = {
+                type: 'add_item'
+            };
+            dispatch(action);
+        },
+        handleDel(index){
+            //console.log(this);
+            const action = {
+                type: 'del_item',
+                index: index
+            };
+            dispatch(action);
+        }
     }
 };
-//怎么做连接？这是映射关系
+//怎么做连接？这是映射关系。store的inputValue映射到props到inputValue里面去
 const mapStateToProps = (state) => {
     return {
-        inputValue: state.inputValue
+        inputValue: state.inputValue,
+        list: state.list
     }
 };
 //TodoList组件和store进行连接
 export default connect(mapStateToProps,mapDispatchToProps)(TodoList);
+//TodoList是个UI组件，用connect返回的就是一个容器组件了
--- todolist/src/store/reducer.js
+++ todolist/src/store/reducer.js
@@ -1,7 +1,24 @@
 const defaultState = {
-    inputValue: 'hello?',
+    inputValue: '',
     list: []
 };
 export default (state = defaultState, action) => {
+    if(action.type === 'change_input_value'){
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.inputValue = action.value;
+        return newState
+    }
+    if(action.type === 'add_item'){
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.list.push(newState.inputValue);
+        newState.inputValue = '';
+        return newState
+    }
+    if(action.type === 'del_item' ){
+        const newState = JSON.parse(JSON.stringify(state));
+        console.log(action.index);
+        newState.list.splice(action.index, 1);
+        return newState
+    }
     return state;
 }
